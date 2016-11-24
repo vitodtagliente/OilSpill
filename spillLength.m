4a8e169127b43e15a4d73b698a9b1e8340d4a4ba
Luke <pippoluca90@hotmail.it>
2016-11-24 15:40:02 +0100

    fix

--- spillLength.m
+++ spillLength.m
@@ -8,17 +8,27 @@ x=x'; % la funzione delaunayTriangolation vuole vettori colonna
 y=y';
 DT = delaunayTriangulation(x,y);
 triplot(DT);
-%axis equal
-%xlabel('Longitude'), ylabel('Latitude')
-%grid on
-%hold on
-k = convexHull(DT);
-xHull = DT.Points(k,1);
+axis equal
+xlabel('Longitude'), ylabel('Latitude')
+grid on
+hold on
+k = convexHull(DT);%Contiene l'indice di riga degli elementi del vettore DT.Points che definiscono il contorno.
+xHull = DT.Points(k,1);%Crea il vettore di ascisse con i punti del vettore DT.Points
 yHull = DT.Points(k,2);
-%plot(xHull,yHull,'r','LineWidth',2);
-%hold off
+plot(xHull,yHull,'r','LineWidth',2);
+hold off
 d=0;
-for i=1 : size(xHull)-1 % ciclo che calcola la distanza euclidea tra due punti usando i triangoli più esterni
+for i=1 : size(xHull)-1 % ciclo che calcola la distanza euclidea tra due punti usando la distanza euclidea
     
     d= d + sqrt((xHull(i)-xHull(i+1))^2 + (yHull(i)-yHull(i+1))^2);
 end
+
+ti = edgeAttachments(DT,k(1),k(2));
+ti{:};%righe della connectivity List contenente il record dei vertici del triangolo
+val= DT.ConnectivityList(ti);
+val{:};
+
+
+
+
+
