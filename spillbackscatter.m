a4b589bf1d51801ad82826748094154c8510f38d
Vito Domenico Tagliente <hackjoker@hotmail.it>
2016-11-24 15:29:09 +0100

    sculata

--- spillbackscatter.m
+++ spillbackscatter.m
@@ -1,11 +1,38 @@
 
-function [ isd, osd ] = spillbackscatter( spill, back )
+function [ out ] = spillbackscatter( spill, back )
 % Inside Slick Standard Deviation
-isd = std( double( spill ) );
-[a b] = size( isd );
-isd = sum( isd ) / b;
+out.isd = mean (std( double( spill ) ) );
+
+% Inside Slick Radar Backcatter
+out.imd = mean( mean( spill ) );
 
 % Outside Slick Standard Deviation (?sce)
-osd = std( double( back ) );
-[a b] = size( osd );
-osd = sum( osd ) / b;
+out.osd = mean( std( double( back ) ) );
+
+% Outside Slick Radar Backscatter (?sce)
+out.omd = mean( mean( back ) );
+
+% Intensity Ratio
+out.ir = out.imd / out.omd;
+
+% Intensity Standard Deviation Ratio
+out.isdr = out.isd / out.osd;
+
+% Intensity Standard Deviation Ratio Inside (ISRI)
+out.isri = out.imd / out.isd;
+
+% Intensity Standard Deviation Ratio Outside (ISRO)
+out.isro = out.omd / out.osd;
+
+% ISRI ISRO Ratio
+out.iratio = out.isri / out.isro;
+
+
+
+
+% Mean Contrast (ConMe) 
+% difference (in dB) between the background mean
+% value and the object mean value
+
+out.conme = out.omd - out.imd;
+
