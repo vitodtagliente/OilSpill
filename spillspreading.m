57967f529676af26d056f362c6c2593919e28f0f
Vito Domenico Tagliente <hackjoker@hotmail.it>
2016-11-24 13:40:07 +0100

    spreding fix

--- spillspreading.m
+++ spillspreading.m
@@ -6,7 +6,22 @@
 % long and thin objects and high for objects closer 
 % to a circular shape.
 
+% It is derived from the principal components analysis
+% [6] of the vectors whose components are the
+% coordinates of the pixels belonging to the object. 
+% If ?1 and ?2 are the two eigenvalues associated 
+% with the computed covariance matrix and ?1> ?2
+% the spreading value S is computed using 
+% the expression:
+% S = 100?2 /( ?1 + ?2 ) 
+
 function [result] = spillspreading( img )
 % Lavoriamo solo con i pixel del contorno
-[length, perim] = spillperim( img );
+[length, pimg] = spillperim( img );
+
+% Calcolo della matrice di covarianza
+% c = cov( pimg );
+
+% Calcolo degli autovalori
+% e = eig( c );
 
