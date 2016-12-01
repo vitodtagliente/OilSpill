e1ce9efd81361d5e1b7b55ac40e7018840d49f91
Vito Domenico Tagliente <hackjoker@hotmail.it>
2016-12-01 16:32:08 +0100

    doc added

--- spillbegin.m
+++ spillbegin.m
@@ -1,17 +1,16 @@
 
-% Utilizza questo script per caricare e 
-% segmentare l'immagine in input
+% Utilizza questo script per preparare il Workspace di Matlab
 
 % img: immagine originale
 img = imread( 'oil.bmp' );
-% gray: immagine non segmentata in scala di grigi
+% gimg: immagine non segmentata in scala di grigi
 gimg = rgb2gray( img );
 % s: immagine segmentata
 s = spillseg( img );
 
-% Calcolo dell'immagine di background
+% back: immagine di background
 back = spillback( img, s );
-% Immagine di background in scala di grigi
+% gback: immagine di background in scala di grigi
 gback = rgb2gray( back );
-
+% gspill: macchia di petrolio in scala di grigi
 gspill = gimg - gback;
